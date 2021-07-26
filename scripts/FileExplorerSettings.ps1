2b2d2813ab2f8064955206039e7a7fbd23a7f1cb
Christian Kunis <ckunis98@gmail.com>
2021-07-26 12:34:17 -0400

    Get desktop path via user libraries

--- scripts/FileExplorerSettings.ps1
+++ scripts/FileExplorerSettings.ps1
@@ -18,7 +18,7 @@ If (-not(Test-Path 'C:\Users\Public\Desktop\Public')) {
 	New-Item -Path 'C:\Users\Public\Desktop\Public' -ItemType Directory -Verbose -ErrorAction SilentlyContinue
 }
 
-$LocalDesktopPath = (Join-Path "$env:USERPROFILE" '\Desktop\Local')
+$LocalDesktopPath = (Join-Path ((Get-LibraryNames).Desktop) '\Local')
 If (-not(Test-Path "$LocalDesktopPath")) {
 	New-Item -Path "$LocalDesktopPath" -ItemType Directory -Verbose -ErrorAction SilentlyContinue
 }
