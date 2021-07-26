d9e5cbb782a84ee0fee72c719e6cf49c2118187b
Christian Kunis <ckunis98@gmail.com>
2021-07-25 22:22:18 -0400

    Changed boxstarter fork

--- scripts/GetFavoriteProjects.ps1
+++ scripts/GetFavoriteProjects.ps1
@@ -3,10 +3,10 @@ git config --global gpg.program $(Resolve-Path (Get-Command gpg | Select-Object 
 Set-Location C:\Github
 # Feel free to customize this with your own preferred projects
 git clone https://github.com/NerdyGriffin/Boxstarter-Scripts
+git clone https://github.com/NerdyGriffin/windows-dev-box-setup-scripts
 git clone https://github.com/PowerShell/PowerShell
 git clone https://github.com/chocolatey-community/chocolatey-coreteampackages.git
 git clone https://github.com/gordon-cs/gordon-360-api
 git clone https://github.com/gordon-cs/gordon-360-ui
 git clone https://github.com/microsoft/winappdriver
-git clone https://github.com/microsoft/windows-dev-box-setup-scripts
 git clone https://github.com/microsoft/wsl
