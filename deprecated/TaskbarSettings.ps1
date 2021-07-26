f7aa0e89893fef7f4d691674e5b9bf6a2d0025bf
Christian Kunis <ckunis98@gmail.com>
2021-07-25 22:59:03 -0400

    Removed deprecated folder

--- deprecated/TaskbarSettings.ps1
+++
@@ -1,3 +0,0 @@
-#--- Configuring Windows properties ---
-#--- Windows Features ---
-Set-BoxstarterTaskbarOptions -Lock -NoAutoHide -AlwaysShowIconsOff -Size Large -Dock Bottom -Combine Never -MultiMonitorOn -MultiMonitorMode Open -MultiMonitorCombine Never
