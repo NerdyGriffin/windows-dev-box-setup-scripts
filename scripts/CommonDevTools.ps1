774c32e4c5bc55bfe17ae09012dce774948c6215
Christian Kunis <ckunis98@gmail.com>
2021-07-26 13:11:10 -0400

    Move custom additions out of CommonDevTools

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -2,8 +2,6 @@
 # tools we expect devs across many scenarios will want
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y gitkraken
-choco install -y ssh-copy-id
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
