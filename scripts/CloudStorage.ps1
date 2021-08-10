f0ff9ca1719100a7c811d538250ec78390a38085
Christian Kunis <ckunis98@gmail.com>
2021-08-10 15:28:40 -0400

    Trying more concise method of preventing nextcloud install reboot loop

--- scripts/CloudStorage.ps1
+++ scripts/CloudStorage.ps1
@@ -1,10 +1,14 @@
 choco install -y google-drive-file-stream
 # choco install -y megasync
 
-$NextcloudPlaceholder = Join-Path (Get-LibraryNames).Desktop 'nextcloud-placeholder'
-if (Test-Path $NextcloudPlaceholder) {
-	Remove-Item -Path $NextcloudPlaceholder
-} else {
-	New-Item -Path $NextcloudPlaceholder -ItemType File -Value 'This is just a marker for the script. You may delete this file'
+if (-not(choco list -l nextcloud-client | Select-String -Pattern 'nextcloud-client')) {
 	choco install -y nextcloud-client
 }
+
+# $NextcloudPlaceholder = Join-Path (Get-LibraryNames).Desktop 'nextcloud-placeholder'
+# if (Test-Path $NextcloudPlaceholder) {
+# 	Remove-Item -Path $NextcloudPlaceholder
+# } else {
+# 	New-Item -Path $NextcloudPlaceholder -ItemType File -Value 'This is just a marker for the script. You may delete this file'
+# 	choco install -y nextcloud-client
+# }
