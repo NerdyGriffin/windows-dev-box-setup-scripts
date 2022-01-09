c1c6eaf48ebc2eb3ef31284f310f1c9fdd15327f
Christian Kunis <ckunis98@gmail.com>
2022-01-08 19:17:33 -0500

    Fixed error caused by missing log file on desktop

--- nerdygriffin_daily_driver.ps1
+++ nerdygriffin_daily_driver.ps1
@@ -71,6 +71,9 @@ Disable-BingSearch
 
 #--- Parse Boxstarter log for failed package installs ---
 $FailuresLog = (Join-Path ((Get-LibraryNames).Desktop) '\boxstarter-failures.log')
+If (-not(Test-Path $FailuresLog)) {
+	New-Item -Path $FailuresLog -ItemType File | Out-Null
+}
 Get-Content -Path $Boxstarter.Log | Select-String -Pattern '^Failures$' -Context 0, 2 | ForEach-Object {
 	$FirstLine = $_.Context.PostContext[0]
 	$SplitString = $FirstLine.split()
