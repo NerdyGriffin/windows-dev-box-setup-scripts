91ffa73b2ad40da1bbae561dc78da9e1a3ace525
Christian Kunis <ckunis98@gmail.com>
2022-01-31 04:15:11 -0500

    Install windows terminal via winget

--- scripts/ConfigurePowerShell.ps1
+++ scripts/ConfigurePowerShell.ps1
@@ -8,7 +8,8 @@ refreshenv
 # choco install -y firacodenf
 
 #--- Windows Terminal ---
-choco upgrade -y microsoft-windows-terminal; choco upgrade -y microsoft-windows-terminal # Does this twice because the first attempt often fails but leaves the install partially completed, and then it completes successfully the second time.
+winget install --id=Microsoft.WindowsTerminal  -e
+# choco upgrade -y microsoft-windows-terminal; choco upgrade -y microsoft-windows-terminal # Does this twice because the first attempt often fails but leaves the install partially completed, and then it completes successfully the second time.
 
 #--- Enable Powershell Script Execution
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force -ErrorAction Continue
