60b2d8bc2f23a4d0b22f6255ec89518de986202b
Christian Kunis <ckunis98@gmail.com>
2022-02-05 16:17:51 -0500

    Install Winget if needed

--- scripts/ConfigurePowerShell.ps1
+++ scripts/ConfigurePowerShell.ps1
@@ -7,6 +7,23 @@ refreshenv
 # choco install -y cascadia-code-nerd-font
 # choco install -y firacodenf
 
+#--- Winget ---
+# Check if winget is installed
+if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
+	'Winget Already Installed'
+} else {
+	# Installing winget from the Microsoft Store
+	Write-Host "Winget not found, installing it now."
+	$ResultText.text = "`r`n" + "`r`n" + "Installing Winget... Please Wait"
+	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+	$nid = (Get-Process AppInstaller).Id
+	Wait-Process -Id $nid
+	Write-Host Winget Installed
+	$ResultText.text = "`r`n" + "`r`n" + "Winget Installed - Ready for Next Task"
+}
+refreshenv
+Start-Sleep -Seconds 1;
+
 #--- Windows Terminal ---
 winget install --id=Microsoft.WindowsTerminal -e --accept-source-agreements
 # choco upgrade -y microsoft-windows-terminal; choco upgrade -y microsoft-windows-terminal # Does this twice because the first attempt often fails but leaves the install partially completed, and then it completes successfully the second time.
