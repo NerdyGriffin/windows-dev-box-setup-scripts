7bd6e01993ad323a45089e062388b88ec109cf1e
Christian Kunis <ckunis98@gmail.com>
2022-02-05 14:46:14 -0500

    Create scheduled task to run winget upgrade daily

--- scripts/PackageManagement.ps1
+++ scripts/PackageManagement.ps1
@@ -1,3 +1,16 @@
 #--- Unofficial Chocolatey Tools ---
 choco install -y choco-package-list-backup
 choco install -y choco-upgrade-all-at
+
+#--- Winget Automatic Scheduled Updates ---
+$STAction = New-ScheduledTaskAction -Execute 'winget' -Argument 'upgrade --all'
+$STTrigger = New-ScheduledTaskTrigger -Daily -At 2am
+$STPrin = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\SYSTEM' -RunLevel Highest
+$STSetings = New-ScheduledTaskSettingsSet
+
+if (Get-ScheduledTask -TaskName 'WingetUpgradeAllTask' -ErrorAction SilentlyContinue) {
+	Set-ScheduledTask -TaskName 'WingetUpgradeAllTask' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
+} else {
+	Register-ScheduledTask -TaskName 'WingetUpgradeAllTask' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
+}
+Clear-Variable STAction, STPrin, STSetings, STTrigger
