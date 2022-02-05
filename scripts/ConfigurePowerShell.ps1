171b1875d8c0d3131a2786f2026072d36a809569
Christian Kunis <ckunis98@gmail.com>
2022-02-05 17:33:41 -0500

    Fix mis-match in copied code and provide link to original source

--- scripts/ConfigurePowerShell.ps1
+++ scripts/ConfigurePowerShell.ps1
@@ -8,18 +8,18 @@ refreshenv
 # choco install -y firacodenf
 
 #--- Winget ---
-# Check if winget is installed
+# Check if winget is installed (source: https://github.com/ChrisTitusTech/win10script)
 if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
 	'Winget Already Installed'
 } else {
 	# Installing winget from the Microsoft Store
 	Write-Host "Winget not found, installing it now."
-	$ResultText.text = "`r`n" + "`r`n" + "Installing Winget... Please Wait"
+	Write-Verbose "`r`n" + "`r`n" + "Installing Winget... Please Wait"
 	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
 	$nid = (Get-Process AppInstaller).Id
 	Wait-Process -Id $nid
 	Write-Host Winget Installed
-	$ResultText.text = "`r`n" + "`r`n" + "Winget Installed - Ready for Next Task"
+	Write-Verbose "`r`n" + "`r`n" + "Winget Installed - Ready for Next Task"
 }
 refreshenv
 Start-Sleep -Seconds 1;
