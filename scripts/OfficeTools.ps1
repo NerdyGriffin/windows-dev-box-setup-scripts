30564187aace1233ba948e5069eb6ee38d3fdbd6
Christian Kunis <ckunis98@gmail.com>
2022-02-05 12:32:00 -0500

    Switch to winget for powertoys, LibreOffice, and Microsoft Whiteboard

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -3,7 +3,7 @@ choco install -y adobereader
 choco install -y pdfsam
 
 #--- Office Suite ---
-choco install -y libreoffice-fresh
+winget install --id=TheDocumentFoundation.LibreOffice  -e
 
 #--- Handwritten Document & Drawing ---
 choco install -y xournalplusplus
@@ -13,4 +13,4 @@ choco install -y calibre
 choco install -y kindle
 
 #--- Other ---
-winget install 'Microsoft Whiteboard'
+winget install --id=9MSPC6MP8FM4  -e # Microsoft Whiteboard
--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -2,7 +2,7 @@
 choco install -y autoruns
 # choco install -y everything
 # choco install -y mousewithoutborders
-choco install -y powertoys
+winget install --id=Microsoft.PowerToys  -e
 choco install -y plasso --ignore-checksums # The checksums are never correct on this package, that is to be expected
 choco install -y reshack
 choco install -y shutup10
