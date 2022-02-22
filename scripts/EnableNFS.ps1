fbad808dc99cf982ea25b46ee3d2eea797ded788
Christian Kunis <ckunis98@gmail.com>
2022-02-22 05:32:05 -0500

    Fixed errors in scheduled task creation

--- scripts/EnableNFS.ps1
+++ scripts/EnableNFS.ps1
@@ -11,41 +11,27 @@ nfsadmin client start
 nfsadmin client localhost config fileaccess=755 SecFlavors=+sys -krb5 -krb5i
 Write-Host "NFS is now setup for user based NFS mounts"
 
-try {
-	Invoke-Expression '\\files.nerdygriffin.net\scripts\MountNFSShares.bat'
-} catch {
-	try {
-		Invoke-Expression '\\files.nerdygriffin.net\scripts\MountNFSShares.ps1'
-	} catch {}
-}
-
 $SMBScriptsPath = '\\files.nerdygriffin.net\scripts'
-try {
-	If ((Test-Path $SMBScriptsPath) -and (Test-Path '\\nfs.nerdygriffin.net\mnt\user\scripts')) {
+If ((Test-Path $SMBScriptsPath) -and (Test-Path '\\nfs.nerdygriffin.net\mnt\user\scripts')) {
 
-		$MountNFSBatch = 'MountNFSShares.bat'
+	$MountNFSBatch = 'MountNFSShares.bat'
 
-		$MountNFSBatchLocalPath = (Join-Path $env:ProgramData $MountNFSBatch)
-		$MountNFSBatchRemotePath = (Join-Path $SMBScriptsPath $MountNFSBatch)
+	$MountNFSBatchLocalPath = (Join-Path $env:ProgramData $MountNFSBatch)
+	$MountNFSBatchRemotePath = (Join-Path $SMBScriptsPath $MountNFSBatch)
 
-		if (Test-Path $MountNFSBatchRemotePath) {
-			Copy-Item -Path $MountNFSBatchRemotePath -Destination $MountNFSBatchLocalPath -Force
+	If (Test-Path $MountNFSBatchRemotePath) {
+		Copy-Item -Path $MountNFSBatchRemotePath -Destination $MountNFSBatchLocalPath -Force
 
-			$STAction = New-ScheduledTaskAction - -Execute "$BackupFFSBatchLocalPath"
-			$STTrigger = New-ScheduledTaskTrigger -AtStartup
-			$STPrin = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\SYSTEM' -RunLevel Highest
-			$STSetings = New-ScheduledTaskSettingsSet
+		$STAction = New-ScheduledTaskAction -Execute "$MountNFSBatchLocalPath"
+		$STTrigger = New-ScheduledTaskTrigger -AtStartup
+		$STPrin = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\SYSTEM' -RunLevel Highest
+		$STSetings = New-ScheduledTaskSettingsSet
 
-			if (Get-ScheduledTask -TaskName 'MountNFSShares' -ErrorAction SilentlyContinue) {
-				Set-ScheduledTask -TaskName 'MountNFSShares' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
-			} else {
-				Register-ScheduledTask -TaskName 'MountNFSShares' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
-			}
-			Clear-Variable STAction, STPrin, STSetings, STTrigger
+		If (Get-ScheduledTask -TaskName 'MountNFSShares' -ErrorAction SilentlyContinue) {
+			Set-ScheduledTask -TaskName 'MountNFSShares' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
+		} else {
+			Register-ScheduledTask -TaskName 'MountNFSShares' -Action $STAction -Principal $STPrin -Settings $STSetings -Trigger $STTrigger
 		}
+		Clear-Variable STAction, STPrin, STSetings, STTrigger
 	}
-} catch {
-	Write-Warning "An error occurred while attempting to setup NFS. This script will be skipped."
-} finally {
-
 }
