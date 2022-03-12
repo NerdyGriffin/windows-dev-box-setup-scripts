3a7d9c9bb1962785c587f342410c72919eef0363
Christian Kunis <ckunis98@gmail.com>
2022-03-12 12:14:51 -0500

    Set NFS task to run script directly off of NFS share

--- scripts/EnableNFS.ps1
+++ scripts/EnableNFS.ps1
@@ -12,17 +12,18 @@ nfsadmin client localhost config fileaccess=755 SecFlavors=+sys -krb5 -krb5i
 Write-Host "NFS is now setup for user based NFS mounts"
 
 $SMBScriptsPath = '\\files.nerdygriffin.net\scripts'
-If ((Test-Path $SMBScriptsPath) -and (Test-Path '\\nfs.nerdygriffin.net\mnt\user\scripts')) {
+$NFSScriptsPath = '\\nfs.nerdygriffin.net\mnt\user\scripts'
+If ((Test-Path $SMBScriptsPath) -and (Test-Path $NFSScriptsPath)) {
 
 	$MountNFSBatch = 'MountNFSShares.bat'
 
-	$MountNFSBatchLocalPath = (Join-Path $env:ProgramData $MountNFSBatch)
-	$MountNFSBatchRemotePath = (Join-Path $SMBScriptsPath $MountNFSBatch)
+	# $MountNFSBatchLocalPath = (Join-Path $env:ProgramData $MountNFSBatch)
+	$MountNFSBatchRemotePath = (Join-Path $NFSScriptsPath $MountNFSBatch)
 
 	If (Test-Path $MountNFSBatchRemotePath) {
-		Copy-Item -Path $MountNFSBatchRemotePath -Destination $MountNFSBatchLocalPath -Force
+		# Copy-Item -Path $MountNFSBatchRemotePath -Destination $MountNFSBatchLocalPath -Force
 
-		$STAction = New-ScheduledTaskAction -Execute "$MountNFSBatchLocalPath"
+		$STAction = New-ScheduledTaskAction -Execute "$MountNFSBatchRemotePath"
 		$STTrigger = New-ScheduledTaskTrigger -AtLogOn
 		$STPrin = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\SYSTEM' -RunLevel Highest
 		$STSetings = New-ScheduledTaskSettingsSet
