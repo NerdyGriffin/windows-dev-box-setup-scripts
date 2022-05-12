3ed96273eb26a136c53970f34ff1f15b3eac7b50
Christian Kunis <ckunis98@gmail.com>
2022-05-12 19:20:21 -0400

    Added Inkscape to list of multimedia programs

--- scripts/Multimedia.ps1
+++ scripts/Multimedia.ps1
@@ -6,6 +6,7 @@ choco install -y foobar2000
 choco install -y freeencoderpack
 choco install -y fsviewer
 choco install -y gimp
+choco install -y inkscape
 choco install -y k-litecodecpackfull
 choco install -y lame
 choco install -y obs-studio
