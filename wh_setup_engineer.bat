4ff04631c92e289f11ea129daa9f5324c2587909
Christian Kunis <engineering@lixiehammers.com>
2022-06-02 15:01:25 -0400

    Removed WHC batch files

--- wh_setup_engineer.bat
+++
@@ -1,3 +0,0 @@
-ECHO OFF
-START microsoft-edge:http://boxstarter.org/package/url?https://raw.githubusercontent.com/NerdyGriffin/windows-dev-box-setup-scripts/master/wh_engineer.ps1
-PAUSE
--- wh_setup_workstation.bat
+++
@@ -1,3 +0,0 @@
-ECHO OFF
-START microsoft-edge:http://boxstarter.org/package/url?https://raw.githubusercontent.com/NerdyGriffin/windows-dev-box-setup-scripts/master/wh_workstation.ps1
-PAUSE
