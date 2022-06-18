73e10f522388f7bfb2c0e1dc98f103c7ca17b0e5
Christian Kunis <ckunis98@gmail.com>
2022-06-18 17:10:23 -0400

    Moved boxstarter log code to helper script

--- nerdygriffin_sysadmin.ps1
+++ nerdygriffin_sysadmin.ps1
@@ -58,18 +58,7 @@ executeScript 'RemoteAndLocalFileSystem.ps1';
 executeScript 'CustomBackup.ps1';
 
 #--- Parse Boxstarter log for failed package installs ---
-$FailuresLog = (Join-Path ((Get-LibraryNames).Desktop) '\boxstarter-failures.log')
-If (-not(Test-Path $FailuresLog)) {
-	New-Item -Path $FailuresLog -ItemType File | Out-Null
-}
-Get-Content -Path $Boxstarter.Log | Select-String -Pattern '^Failures$' -Context 0, 2 | ForEach-Object {
-	$FirstLine = $_.Context.PostContext[0]
-	$SplitString = $FirstLine.split()
-	$PackageName = $SplitString[2]
-	if (-not(Select-String -Pattern $PackageName -Path $FailuresLog )) {
-		Add-Content -Path $FailuresLog -Value $_.Context.PostContext
-	}
-}
+executeScript 'ParseBoxstarterLog.ps1';
 
 #--- reenabling critial items ---
 Enable-UAC
