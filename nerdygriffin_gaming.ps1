7bb5040839d29fbae3cfb48309100b14e95ff98f
Christian Kunis <ckunis98@gmail.com>
2022-06-18 17:06:48 -0400

    Create boxstart-failures.log if it does not already exists

--- nerdygriffin_gaming.ps1
+++ nerdygriffin_gaming.ps1
@@ -77,6 +77,9 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 
 #--- Parse Boxstarter log for failed package installs ---
 $FailuresLog = (Join-Path ((Get-LibraryNames).Desktop) '\boxstarter-failures.log')
+If (-not(Test-Path $FailuresLog)) {
+	New-Item -Path $FailuresLog -ItemType File | Out-Null
+}
 Get-Content -Path $Boxstarter.Log | Select-String -Pattern '^Failures$' -Context 0, 2 | ForEach-Object {
 	$FirstLine = $_.Context.PostContext[0]
 	$SplitString = $FirstLine.split()
