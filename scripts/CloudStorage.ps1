2c28a0a5a19fc02146ead6f11b7fda910e2a4949
Christian Kunis <ckunis98@gmail.com>
2022-11-11 14:55:09 -0500

    Test nextcloud install via choco

--- scripts/CloudStorage.ps1
+++ scripts/CloudStorage.ps1
@@ -2,15 +2,17 @@
 # Install-WinGetApp -Id 'Google.Drive'
 # choco install -y megasync
 
-$NextcloudCheck = $(choco list -l nextcloud-client | Select-String -Pattern 'nextcloud-client')
-if (-not($NextcloudCheck)) {
-	choco install -y nextcloud-client
-}
+choco install -y nextcloud-client
 
-$NextcloudPlaceholder = Join-Path (Get-LibraryNames).Desktop 'nextcloud-placeholder'
-if (Test-Path $NextcloudPlaceholder) {
-	Remove-Item -Path $NextcloudPlaceholder
-} else {
-	New-Item -Path $NextcloudPlaceholder -ItemType File -Value 'This is just a marker for the script. You may delete this file'
-	choco install -y nextcloud-client
-}
+# $NextcloudCheck = $(choco list -l nextcloud-client | Select-String -Pattern 'nextcloud-client')
+# if (-not($NextcloudCheck)) {
+# 	choco install -y nextcloud-client
+# }
+
+# $NextcloudPlaceholder = Join-Path (Get-LibraryNames).Desktop 'nextcloud-placeholder'
+# if (Test-Path $NextcloudPlaceholder) {
+# 	Remove-Item -Path $NextcloudPlaceholder
+# } else {
+# 	New-Item -Path $NextcloudPlaceholder -ItemType File -Value 'This is just a marker for the script. You may delete this file'
+# 	choco install -y nextcloud-client
+# }
