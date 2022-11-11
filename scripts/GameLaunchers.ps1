c119cb3edddea01bd123fd25c348c97ee7b451ba
Christian Kunis <ckunis98@gmail.com>
2022-11-11 14:54:40 -0500

    Add extra commandline options to EA Desktop app

--- scripts/GameLaunchers.ps1
+++ scripts/GameLaunchers.ps1
@@ -2,7 +2,7 @@
 choco install -y cemu
 choco install -y dolphin
 choco install -y ds4windows
-winget install --id=ElectronicArts.EADesktop  -e
+winget install --id=ElectronicArts.EADesktop --exact --accept-package-agreements --accept-source-agreements
 choco install -y epicgameslauncher
 choco install -y goggalaxy
 choco install -y minecraft-launcher
