5babc8519dbe761990242fa49ee568de70568b40
Christian Kunis <ckunis98@gmail.com>
2022-11-11 14:41:18 -0500

    Remove winget from Git config script

--- scripts/ConfigureGit.ps1
+++ scripts/ConfigureGit.ps1
@@ -1,26 +1,6 @@
-Function Install-WinGetApp {
-	param([Parameter(Mandatory = $true, Position = 0)][string]$Id,
-		[Parameter(Mandatory = $false, Position = 1)][string]$Source)
-	#check if the app is already installed
-	$listApp = winget list --exact -q $Id
-	if (![String]::Join("", $listApp).Contains($Id)) {
-		Write-Host "Installing:" $Id
-		if ($Source -ne $null) {
-			winget install --exact --silent $Id --source $Source --accept-package-agreements --accept-source-agreements
-		} else {
-			winget install --exact --silent $Id --accept-package-agreements --accept-source-agreements
-		}
-		RefreshEnv;
-	} else {
-		Write-Host "Skipping Install of " $Id
-	}
-	Start-Sleep -Seconds 1;
-}
-
 if (([Security.Principal.WindowsPrincipal] `
 			[Security.Principal.WindowsIdentity]::GetCurrent() `
 	).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	#Install-WinGetApp -Id 'GnuPG.GnuPG'
 	choco install -y gnupg
 	choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
 	choco install -y gitkraken
