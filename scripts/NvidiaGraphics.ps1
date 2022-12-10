787521bd2137e41abacab32fcaa67ec6d0df761b
Christian Kunis <ckunis98@gmail.com>
2022-12-10 10:33:31 -0500

    Try disabling automatic reboot during geforce installation

--- scripts/NvidiaGraphics.ps1
+++ scripts/NvidiaGraphics.ps1
@@ -1,3 +1,8 @@
+$PrevRebootState = $Boxstarter.RebootOk || $false
+$Boxstarter.RebootOk = $false
+
 #--- Nvidia Graphics ---
 choco install -y geforce-experience
 choco install -y nvidia-profile-inspector
+
+$Boxstarter.RebootOk = $PrevRebootState
