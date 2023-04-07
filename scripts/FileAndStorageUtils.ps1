85ebd3e18ce576c8cc5f36c1798a3f0686d74aec
Christian Kunis <ckunis98@gmail.com>
2023-04-06 20:02:39 -0400

    Comment out Folder Size (replaced by windirstat)

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -2,7 +2,7 @@
 choco install -y 7zip
 choco install -y etcher
 choco install -y filebot
-choco install -y Folder_Size
+# choco install -y Folder_Size
 choco install -y freefilesync
 choco install -y partitionwizard
 choco install -y rufus
