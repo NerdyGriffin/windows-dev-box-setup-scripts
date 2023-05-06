85986e891a6e7c15094c3b67dce983ea5a11380e
Christian Kunis <ckunis98@gmail.com>
2023-05-06 09:26:09 -0400

    Switch from Libre to OnlyOffice as default

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -3,15 +3,15 @@ choco install -y adobereader
 choco install -y pdfsam
 
 #--- Office Suite ---
-choco install -y libreoffice-fresh
-# choco install -y onlyoffice
+# choco install -y libreoffice-fresh
+choco install -y onlyoffice
 
 #--- Handwritten Document & Drawing ---
 choco install -y xournalplusplus
 
 #--- E-Books ---
 choco install -y calibre
-choco install -y kindle
+# choco install -y kindle
 
 #--- Other ---
-winget install --id=9MSPC6MP8FM4  --exact --silent --accept-package-agreements --accept-source-agreements # Microsoft Whiteboard
+winget install --id=9MSPC6MP8FM4 --exact --silent --accept-package-agreements --accept-source-agreements # Microsoft Whiteboard
