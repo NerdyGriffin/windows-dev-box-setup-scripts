399a2c347b63c9fd19ef1ddd7a5489da287b2fc9
Christian Kunis <ckunis98@gmail.com>
2023-05-28 17:00:40 -0400

    Comment out powertoys and sharex, no longer needed

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -3,22 +3,22 @@ choco install -y autoruns
 # choco install -y everything
 # choco install -y mousewithoutborders
 # winget install --id=Microsoft.PowerToys --exact --silent --accept-package-agreements --accept-source-agreements
-choco install -y powertoys
+# choco install -y powertoys
 choco install -y plasso --ignore-checksums # The checksums are never correct on this package, that is to be expected
 choco install -y reshack
 choco install -y shutup10
-choco install -y sharex
+# choco install -y sharex
 choco install -y sdio
 choco install -y tcpview --ignore-checksums
 # choco install -y winaero-tweaker
 # choco install -y xyplorer
 
-# Add a custom registry entry for running PowerToys at startup
-$PowerToysCommand = """$env:ProgramFiles\PowerToys\PowerToys.exe"""
-Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name PowerToys -Value $PowerToysCommand
+# # Add a custom registry entry for running PowerToys at startup
+# $PowerToysCommand = """$env:ProgramFiles\PowerToys\PowerToys.exe"""
+# Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name PowerToys -Value $PowerToysCommand
 
-# Add a custom registry entry for running Sharex at startup
-$SharexCommand = """$env:ProgramFiles\Sharex\sharex.exe"" -silent"
-Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
-Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
-# We want Sharex to always startup BEFORE OneDrive (to prevent OneDrive from hooking the "print screen" hotkey)
+# # Add a custom registry entry for running Sharex at startup
+# $SharexCommand = """$env:ProgramFiles\Sharex\sharex.exe"" -silent"
+# Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
+# Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
+# # We want Sharex to always startup BEFORE OneDrive (to prevent OneDrive from hooking the "print screen" hotkey)
