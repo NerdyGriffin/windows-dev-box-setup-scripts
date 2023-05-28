383fd2bb1cbd8bf6f5f3e416fc4f9b04ca751e07
Christian Kunis <ckunis98@gmail.com>
2023-05-28 17:01:01 -0400

    Comment out hardware monitoring, not currently used

--- scripts/HardwareMonitoring.ps1
+++ scripts/HardwareMonitoring.ps1
@@ -1,9 +1,9 @@
 #--- Hardware Monitoring ---
-choco install -y cpu-z
-choco install -y gpu-z
-choco install -y hwinfo
-choco install -y hwmonitor
-choco install -y intel-xtu
-choco install -y latencymon
-choco install -y msiafterburner
-choco install -y msiafterburner --ignore-checksums
+# choco install -y cpu-z
+# choco install -y gpu-z
+# choco install -y hwinfo
+# choco install -y hwmonitor
+# choco install -y intel-xtu
+# choco install -y latencymon
+# choco install -y msiafterburner
+# choco install -y msiafterburner --ignore-checksums
