10318cba08b7d04182e2f7aec1cd8b2aee2d8d7c
Christian Kunis <ckunis98@gmail.com>
2023-05-28 21:27:01 -0400

    Remove partition wizard

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -4,7 +4,7 @@ choco install -y etcher
 choco install -y filebot
 # choco install -y Folder_Size
 choco install -y freefilesync
-choco install -y partitionwizard
+# choco install -y partitionwizard
 choco install -y rufus
 choco install -y teracopy
 choco install -y tuxboot
