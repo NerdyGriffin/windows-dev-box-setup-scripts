5cb10cd3f381f80bef56ddcdccb28d54131c3ff7
Christian Kunis <ckunis98@gmail.com>
2023-06-08 19:34:30 -0400

    Cleanup Windows Power User tools

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -1,24 +1,6 @@
 #--- Windows 10 Tools ---
 choco install -y autoruns
-# choco install -y everything
-# choco install -y mousewithoutborders
-# winget install --id=Microsoft.PowerToys --exact --silent --accept-package-agreements --accept-source-agreements
-# choco install -y powertoys
-choco install -y plasso --ignore-checksums # The checksums are never correct on this package, that is to be expected
+winget install --id=BitSum.ProcessLasso --exact --silent --accept-package-agreements --accept-package-agreements
 choco install -y reshack
 choco install -y shutup10
-# choco install -y sharex
-# choco install -y sdio --ignore-checksums # sdio = Snappy Driver Installer Origin
 # winget install --id=GlennDelahoy.SnappyDriverInstallerOrigin --exact --silent --accept-package-agreements --accept-source-agreements
-# choco install -y winaero-tweaker
-# choco install -y xyplorer
-
-# # Add a custom registry entry for running PowerToys at startup
-# $PowerToysCommand = """$env:ProgramFiles\PowerToys\PowerToys.exe"""
-# Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name PowerToys -Value $PowerToysCommand
-
-# # Add a custom registry entry for running Sharex at startup
-# $SharexCommand = """$env:ProgramFiles\Sharex\sharex.exe"" -silent"
-# Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
-# Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name Sharex -Value $SharexCommand
-# # We want Sharex to always startup BEFORE OneDrive (to prevent OneDrive from hooking the "print screen" hotkey)
