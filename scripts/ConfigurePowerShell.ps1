30b21306e7525ecfe8bfcb138cd8a22ca8d84487
Christian Kunis <ckunis98@gmail.com>
2023-06-10 14:29:01 -0400

    Replace windows terminal settings with symlink to OneDrive

--- scripts/ConfigurePowerShell.ps1
+++ scripts/ConfigurePowerShell.ps1
@@ -173,20 +173,21 @@ powershell -Command $ScriptBlock
 Write-Host 'Configuring PowerShell Core...' -ForegroundColor 'Green'
 pwsh -Command $ScriptBlock
 
-$WindowsTerminalSettingsDir = (Join-Path $env:LOCALAPPDATA '\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState')
-$SymLinkPath = (Join-Path $env:USERPROFILE 'WindowsTerminalSettings')
-if (Test-Path $WindowsTerminalSettingsDir) {
-	if ((-not(Test-Path $SymLinkPath)) -or (-not(Get-Item $SymLinkPath | Where-Object Attributes -Match ReparsePoint))) {
-		New-Item -Path $SymLinkPath -ItemType SymbolicLink -Value $WindowsTerminalSettingsDir -Force -Verbose
-	}
-	$BackupDirName = 'WindowsTerminalSettings.bak'
-	$BackupDirPath = (Join-Path $WindowsTerminalSettingsDir $BackupDirName)
-	if (-not(Test-Path $BackupDirPath)) {
-		New-Item -Path $BackupDirPath -ItemType Directory -Force
-	}
-	if (-not(Test-Path (Join-Path $BackupDirPath 'settings.json'))) {
-		Copy-Item -Path "$WindowsTerminalSettingsDir\*" -Destination $BackupDirPath -Force -Exclude $BackupDirName -Recurse
-	}
+# Replace Windows Terminal settings dir with symlink to OneDrive
+$LocalTermSettingsDir = (Join-Path $env:LOCALAPPDATA '\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState')
+$RemoteTermSettingsDir = (Join-Path $env:OneDrive 'Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState')
+if (-Not(Test-Path $RemoteTermSettingsDir)) {
+	New-Item -Path $RemoteTermSettingsDir -ItemType Directory -Force
 }
+if ((Test-Path $LocalTermSettingsDir) -and (-not(Get-Item $LocalTermSettingsDir | Where-Object Attributes -Match ReparsePoint))) {
+	$BackupPath = "$RemoteTermSettingsDir-$env:COMPUTERNAME-$(Get-Date -Format "yyyy-MM-dd")"
+	if (Test-Path $BackupPath) {
+		Remove-Item -Path $BackupPath -Recurse -Force
+	}
+	New-Item -Path $BackupPath -ItemType Directory -Force
+	Copy-Item -Path "$LocalTermSettingsDir\*" -Destination $BackupPath -Force -Recurse
+	Remove-Item -Path $LocalTermSettingsDir -Recurse -Force
+}
+New-Item -Path $LocalTermSettingsDir -ItemType SymbolicLink -Value $RemoteTermSettingsDir -Force
 
 [System.Environment]::SetEnvironmentVariable('PYTHONSTARTUP', (Join-Path $env:USERPROFILE '.pystartup'))
