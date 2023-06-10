0baabbd6f9ab0bdfc03617f7b908fdffaf1c0e6e
Christian Kunis <ckunis98@gmail.com>
2023-06-10 11:58:11 -0400

    Symlink cura slicer settings to a server share

--- scripts/3DPrinting.ps1
+++ scripts/3DPrinting.ps1
@@ -1,1 +1,13 @@
+# Replace cura settings dir with symlink to server share
+$LocalCuraSettingsPath = (Join-Path $env:APPDATA cura)
+$RemoteCuraSettingsPath = 'P:\3D Printing\Slicer Settings Sync\cura'
+if (Test-Path $LocalCuraSettingsPath) {
+	$BackupPath = "$RemoteCuraSettingsPath-$(Get-Date -Format "yyyy-mm-dd")"
+	if (Test-Path $BackupPath) {
+		Remove-Item -Path $BackupPath -Recurse -Force
+	}
+	Move-Item -Path $LocalCuraSettingsPath -Destination $BackupPath -Force
+}
+New-Item -Path $LocalCuraSettingsPath -ItemType SymbolicLink -Value $RemoteCuraSettingsPath -Force
+
 winget install --id=Ultimaker.Cura --exact --silent --accept-package-agreements --accept-source-agreements
