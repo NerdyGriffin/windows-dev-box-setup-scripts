635e3c3951eee20eef9d22e13126bccf51a3adad
Christian Kunis <ckunis98@gmail.com>
2023-06-10 08:53:54 -0400

    Comment out freefilesync due to installer errors

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -2,9 +2,8 @@
 choco install -y 7zip
 choco install -y etcher
 choco install -y filebot
-# choco install -y Folder_Size
-choco install -y freefilesync
-# choco install -y partitionwizard
+# choco install -y Folder_Size # Replaced by WinDirStat
+# choco install -y freefilesync # Installer possibly broken 2023-06-10
 choco install -y rufus
 choco install -y teracopy
 choco install -y tuxboot
