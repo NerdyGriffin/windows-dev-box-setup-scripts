a7a32586673157d15c4b8dd0108acd063d3f3b9b
Christian Kunis <ckunis98@gmail.com>
2023-07-04 14:31:06 -0400

    Add back custom file explorer settings
    I got annoyed with the new defaults in Windows 11

--- nerdygriffin_all_in_one.ps1
+++ nerdygriffin_all_in_one.ps1
@@ -40,6 +40,7 @@ executeScript 'ConfigureChocolatey.ps1';
 #--- Setting up Windows ---
 executeScript 'SystemConfiguration.ps1';
 # executeScript 'FileExplorerSettings.ps1';
+executeScript 'CustomFileExplorerSettings.ps1'
 # executeScript 'RemoveDefaultApps.ps1';
 executeScript 'CommonDevTools.ps1';
 executeScript 'SetTimeZone.ps1';
---
+++ scripts/CustomFileExplorerSettings.ps1
@@ -0,0 +1,10 @@
+#--- Configuring Windows properties ---
+#--- Windows Features ---
+# Show hidden files, Show protected OS files, Show file extensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -DisableShowFrequentFoldersInQuickAccess -DisableExpandToOpenFolder -EnableShowRibbon -EnableItemCheckBox
+
+#--- File Explorer Settings ---
+#adds things back in your left pane like recycle bin
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+#opens PC to This PC, not quick access
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
