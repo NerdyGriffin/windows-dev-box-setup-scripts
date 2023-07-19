a6fc2a13c56d64a10d1c467b247d4951cd901cb0
Christian Kunis <ckunis98@gmail.com>
2023-07-19 17:09:26 -0400

    Replace WinDirStat with WizTree

--- scripts/FileAndStorageUtils.ps1
+++ scripts/FileAndStorageUtils.ps1
@@ -7,6 +7,7 @@ choco install -y filebot
 choco install -y rufus
 choco install -y teracopy
 choco install -y tuxboot
-choco install -y windirstat
+# choco install -y windirstat # Replaced by WizTree
+choco install -y wiztree
 choco install -y winmerge
 choco install -y winmerge-7z
