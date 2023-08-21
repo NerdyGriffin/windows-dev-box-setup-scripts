9f53bb6b33eba9434c9c401c36a3f10be8ef492b
Christian Kunis <ckunis98@gmail.com>
2023-08-21 17:21:20 -0400

    Re-add firefox (planned to replace Brave Browser)

--- scripts/Browsers.ps1
+++ scripts/Browsers.ps1
@@ -2,4 +2,5 @@
 choco install -y brave
 # choco install -y chromium
 # choco install -y firefox
+winget install --id=Mozilla.Firefox --exact --silent --accept-package-agreements --accept-source-agreements
 choco install -y googlechrome
