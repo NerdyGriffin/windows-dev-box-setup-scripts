4960f7361abacc5d66b0d18c75cb3ee6e3fc1867
Christian Kunis <ckunis98@gmail.com>
2024-01-20 15:38:08 -0500

    Updated favorite github projects

--- scripts/GetFavoriteProjects.ps1
+++ scripts/GetFavoriteProjects.ps1
@@ -8,13 +8,6 @@ function Set-CloneLocation ([String] $Path) {
 Set-CloneLocation 'C:\GitHub'
 
 # Feel free to customize this with your own preferred projects
-Set-CloneLocation 'C:\GitHub\NerdyGriffin'
-git clone https://github.com/NerdyGriffin/Cura
-git clone https://github.com/NerdyGriffin/DSC_v2
-git clone https://github.com/NerdyGriffin/windows-dev-box-setup-scripts
-
-Set-CloneLocation 'C:\GitHub\PowerShell'
-git clone https://github.com/PowerShell/PowerShell
 
 Set-CloneLocation 'C:\GitHub\chocolatey-community'
 git clone https://github.com/chocolatey-community/chocolatey-coreteampackages.git
@@ -27,3 +20,19 @@ Set-CloneLocation 'C:\GitHub\microsoft'
 git clone https://github.com/microsoft/winappdriver
 git clone https://github.com/microsoft/windows-dev-box-setup-scripts
 git clone https://github.com/microsoft/wsl
+
+Set-CloneLocation 'C:\GitHub\NerdyGriffin'
+git clone https://github.com/NerdyGriffin/Cura
+git clone https://github.com/NerdyGriffin/DSC_v2
+git clone https://github.com/NerdyGriffin/USMT.git
+git clone https://github.com/NerdyGriffin/windows-dev-box-setup-scripts
+
+Set-CloneLocation 'C:\GitHub\PowerShell'
+git clone https://github.com/PowerShell/PowerShell
+
+Set-CloneLocation 'C:\GitHub\Ultimaker'
+git clone https://github.com/Ultimaker/Cura.git
+
+Set-CloneLocation 'C:\GitHub\WCEngineer'
+git clone https://github.com/WCEngineer/USMT.git
+git clone https://github.com/WCEngineer/wh-windows-setup-scripts.git
