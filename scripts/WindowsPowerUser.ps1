341392426375225e87b3e850829f2cf8b06f421a
Christian Kunis <ckunis98@gmail.com>
2024-02-03 12:18:08 -0500

    Add RevoUninstaller

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -2,5 +2,6 @@
 choco install -y autoruns
 winget install --id=BitSum.ProcessLasso --exact --silent --accept-package-agreements --accept-package-agreements
 choco install -y reshack
+winget install --id=RevoUninstaller.RevoUninstaller --exact --silent --accept-package-agreements --accept-package-agreements
 choco install -y shutup10
 # winget install --id=GlennDelahoy.SnappyDriverInstallerOrigin --exact --silent --accept-package-agreements --accept-source-agreements
