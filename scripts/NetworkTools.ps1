714fea966c2f19de99c60911efa127b58b6957c5
Christian Kunis <ckunis98@gmail.com>
2024-02-17 16:31:26 -0500

    Remove WinPcap (no longer needed)

--- scripts/NetworkTools.ps1
+++ scripts/NetworkTools.ps1
@@ -2,5 +2,6 @@
 # choco install -y advanced-ip-scanner # Possibly broken for FOSS users
 # choco install -y ipscan # Possibly broken for FOSS users
 # choco install -y tcpview --ignore-checksums
-choco install -y winpcap # Dependency for wireshark
-choco install -y wireshark
+# choco install -y winpcap # Dependency for wireshark
+# choco install -y wireshark
+winget install --id=WiresharkFoundation.Wireshark --exact --silent --accept-package-agreements --accept-package-agreements
