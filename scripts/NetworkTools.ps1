b11daed922416046ea7dcc6de7982d0afd8ecb6e
Christian Kunis <ckunis98@gmail.com>
2024-03-24 19:54:31 -0400

    Add Unifi Identity Desktop application

--- scripts/NetworkTools.ps1
+++ scripts/NetworkTools.ps1
@@ -2,6 +2,7 @@
 # choco install -y advanced-ip-scanner # Possibly broken for FOSS users
 # choco install -y ipscan # Possibly broken for FOSS users
 # choco install -y tcpview --ignore-checksums
+winget install --id=UbiquitiInc.IdentityDesktop --exact --silent --accept-package-agreements --accept-source-agreements
 # choco install -y winpcap # Dependency for wireshark
 # choco install -y wireshark
-winget install --id=WiresharkFoundation.Wireshark --exact --silent --accept-package-agreements --accept-package-agreements
+winget install --id=WiresharkFoundation.Wireshark --exact --silent --accept-package-agreements --accept-source-agreements
