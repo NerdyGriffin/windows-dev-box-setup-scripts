9220a4205e28133c8041fd95c6d9634d7642e3f7
Christian Kunis <ckunis98@gmail.com>
2024-03-24 19:56:39 -0400

    Fix duplicate winget args

--- scripts/WindowsADK.ps1
+++ scripts/WindowsADK.ps1
@@ -1,2 +1,2 @@
-winget install --id=Microsoft.WindowsADK --exact --silent --accept-package-agreements --accept-package-agreements
-winget install --id=Microsoft.DeploymentToolkit --exact --silent --accept-package-agreements --accept-package-agreements
+winget install --id=Microsoft.WindowsADK --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=Microsoft.DeploymentToolkit --exact --silent --accept-package-agreements --accept-source-agreements
--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -1,7 +1,7 @@
 #--- Windows 10 Tools ---
 choco install -y autoruns
-winget install --id=BitSum.ProcessLasso --exact --silent --accept-package-agreements --accept-package-agreements
+winget install --id=BitSum.ProcessLasso --exact --silent --accept-package-agreements --accept-source-agreements
 choco install -y reshack
-winget install --id=RevoUninstaller.RevoUninstaller --exact --silent --accept-package-agreements --accept-package-agreements
+winget install --id=RevoUninstaller.RevoUninstaller --exact --silent --accept-package-agreements --accept-source-agreements
 choco install -y shutup10
 # winget install --id=GlennDelahoy.SnappyDriverInstallerOrigin --exact --silent --accept-package-agreements --accept-source-agreements
