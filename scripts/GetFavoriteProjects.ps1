e20f27962bde37eeae7aed38972be76cdc8f8f6a
Christian Kunis <ckunis98@gmail.com>
2024-06-01 17:02:33 -0400

    Update git repos to use SSH instead of HTTP

--- scripts/GetFavoriteProjects.ps1
+++ scripts/GetFavoriteProjects.ps1
@@ -10,29 +10,29 @@ Set-CloneLocation 'C:\GitHub'
 # Feel free to customize this with your own preferred projects
 
 Set-CloneLocation 'C:\GitHub\chocolatey-community'
-git clone https://github.com/chocolatey-community/chocolatey-coreteampackages.git
+git clone git@github.com:chocolatey-community/chocolatey-coreteampackages.git
 
 Set-CloneLocation 'C:\GitHub\gordon-cs'
-git clone https://github.com/gordon-cs/gordon-360-api
-git clone https://github.com/gordon-cs/gordon-360-ui
+git clone git@github.com:gordon-cs/gordon-360-api
+git clone git@github.com:gordon-cs/gordon-360-ui
 
 Set-CloneLocation 'C:\GitHub\microsoft'
-git clone https://github.com/microsoft/winappdriver
-git clone https://github.com/microsoft/windows-dev-box-setup-scripts
-git clone https://github.com/microsoft/wsl
+git clone git@github.com:microsoft/winappdriver
+git clone git@github.com:microsoft/windows-dev-box-setup-scripts
+git clone git@github.com:microsoft/wsl
 
 Set-CloneLocation 'C:\GitHub\NerdyGriffin'
-git clone https://github.com/NerdyGriffin/Cura
-git clone https://github.com/NerdyGriffin/DSC_v2
-git clone https://github.com/NerdyGriffin/USMT.git
-git clone https://github.com/NerdyGriffin/windows-dev-box-setup-scripts
+git clone git@github.com:NerdyGriffin/Cura
+git clone git@github.com:NerdyGriffin/DSC_v2
+git clone git@github.com:NerdyGriffin/USMT.git
+git clone git@github.com:NerdyGriffin/windows-dev-box-setup-scripts
 
 Set-CloneLocation 'C:\GitHub\PowerShell'
-git clone https://github.com/PowerShell/PowerShell
+git clone git@github.com:PowerShell/PowerShell
 
 Set-CloneLocation 'C:\GitHub\Ultimaker'
-git clone https://github.com/Ultimaker/Cura.git
+git clone git@github.com:Ultimaker/Cura.git
 
 Set-CloneLocation 'C:\GitHub\WCEngineer'
-git clone https://github.com/WCEngineer/USMT.git
-git clone https://github.com/WCEngineer/wh-windows-setup-scripts.git
+git clone git@github.com:WCEngineer/USMT.git
+git clone git@github.com:WCEngineer/wh-windows-setup-scripts.git
