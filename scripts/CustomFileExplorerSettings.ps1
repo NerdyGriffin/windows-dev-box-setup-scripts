d62b3f5a222574c7e1d14a85afc35482117f5ba3
Christian Kunis <ckunis98@gmail.com>
2024-09-07 13:42:57 -0400

    Remove NavPaneShowAllFolders from Explorer.exe changes

--- scripts/CustomFileExplorerSettings.ps1
+++ scripts/CustomFileExplorerSettings.ps1
@@ -4,7 +4,5 @@
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -DisableShowFrequentFoldersInQuickAccess -DisableExpandToOpenFolder -EnableShowRibbon -EnableItemCheckBox
 
 #--- File Explorer Settings ---
-#adds things back in your left pane like recycle bin
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
 #opens PC to This PC, not quick access
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
