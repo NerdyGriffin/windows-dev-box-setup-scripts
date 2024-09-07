20be2d6e14f9bb9c121be6a35da870503402120e
Christian Kunis <ckunis98@gmail.com>
2024-09-07 10:30:41 -0400

    Add start menu shortcut for UKMM

--- scripts/GameModdingTools.ps1
+++ scripts/GameModdingTools.ps1
@@ -13,3 +13,14 @@ refreshenv
 python -m pip install --upgrade pip
 
 powershell -c "Invoke-RestMethod https://github.com/NiceneNerd/ukmm/releases/download/v0.15.0/ukmm-installer.ps1 | Invoke-Expression"
+
+if (Get-Command ukmm -ErrorAction SilentlyContinue) {
+	$UKMMPath = $(Resolve-Path (Get-Command ukmm | Select-Object -Expand Source) | Select-Object -Expand Path)
+	if (Test-Path $UKMMPath) {
+		$UKMMShortcutDest = (Join-Path $env:APPDATA 'Microsoft\Windows\Start Menu\Programs\U-King Mod Manager.lnk')
+		$WshShell = New-Object -ComObject WScript.Shell
+		$Shortcut = $WshShell.CreateShortcut($UKMMShortcutDest)
+		$Shortcut.TargetPath = $UKMMPath
+		$Shortcut.Save()
+	}
+}
