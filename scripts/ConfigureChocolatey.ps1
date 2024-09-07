842bcd17db58f5082652351b2bf7b3127156063c
Christian Kunis <ckunis98@gmail.com>
2024-09-07 09:10:09 -0400

    Fix local choco repository URL

--- scripts/ConfigureChocolatey.ps1
+++ scripts/ConfigureChocolatey.ps1
@@ -1,4 +1,4 @@
 Write-Host 'Configuring Chocolatey to use local Nexus repository'
 # choco source disable --name='chocolatey'
-choco source add --name='nexus' --source='http://nexus.nerdygriffin.net:8081/repository/chocolatey-group/' --priority=1
+choco source add --name='nexus' --source='https://nexus.nerdygriffin.net/repository/chocolatey-group/' --priority=1
 choco config set --name="commandExecutionTimeoutSeconds" --value="14400"
