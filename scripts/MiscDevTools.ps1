b90cad8856e6b9d0a2e5ab72b015d0e5200bae2a
Christian Kunis <ckunis98@gmail.com>
2025-02-08 17:34:14 -0500

    Comment out some more apps I don't use often

--- scripts/MiscDevTools.ps1
+++ scripts/MiscDevTools.ps1
@@ -1,8 +1,8 @@
 #--- Assorted Dev Tools and Dependencies
 # choco install -y HexEdit # ERROR: The remote file either doesn't exist, is unauthorized, or is forbidden for url 'http://www.catch22.net/sites/default/files/hexedit-amd64-2.0.6.49.zip'.
 # choco install -y androidstudio
-choco install -y arduino
-choco install -y arduino-cli
+# choco install -y arduino
+# choco install -y arduino-cli
 # choco install -y electron
 # choco install -y fiddler
 # choco install -y fritzing # Installer is broken
@@ -10,10 +10,10 @@ winget install --id GitHub.cli --exact --silent --accept-package-agreements --ac
 choco install -y gitkraken
 choco install -y hxd
 choco install -y meld
-choco install -y neovim
+# choco install -y neovim
 choco install -y ngrok
 # choco install -y notepadplusplus
 winget install --id=Rustlang.Rustup --exact --silent --accept-package-agreements --accept-source-agreements
 choco install -y ssh-copy-id
-choco install -y vim
+# choco install -y vim
 choco install -y vscode-arduino
