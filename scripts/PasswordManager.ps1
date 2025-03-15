78f15d42510ddc1fd45a7ec0ee57bb6aaef6c9de
Christian Kunis <ckunis98@gmail.com>
2025-03-15 12:04:51 -0400

    Remove more old programs

--- scripts/PasswordManager.ps1
+++ scripts/PasswordManager.ps1
@@ -1,3 +1,2 @@
 #--- Password Manager ---
 choco install -y bitwarden
-# choco install -y keepass
--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -3,5 +3,3 @@ choco install -y autoruns
 winget install --id=BitSum.ProcessLasso --exact --silent --accept-package-agreements --accept-source-agreements
 choco install -y reshack
 winget install --id=RevoUninstaller.RevoUninstaller --exact --silent --accept-package-agreements --accept-source-agreements
-choco install -y shutup10
-# winget install --id=GlennDelahoy.SnappyDriverInstallerOrigin --exact --silent --accept-package-agreements --accept-source-agreements
