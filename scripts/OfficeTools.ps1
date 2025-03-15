3ded06570f3915b9206184312789555e2ecc42d9
Christian Kunis <ckunis98@gmail.com>
2025-03-15 11:09:55 -0400

    cleanup OfficeTools.ps1

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -3,15 +3,7 @@ choco install -y adobereader
 choco install -y pdfsam
 
 #--- Office Suite ---
-# choco install -y libreoffice-fresh
-choco install -y onlyoffice
+winget install --id=ONLYOFFICE.DesktopEditors --exact --silent --accept-package-agreements --accept-source-agreements
 
 #--- Handwritten Document & Drawing ---
 choco install -y xournalplusplus
-
-#--- E-Books ---
-# choco install -y calibre
-# choco install -y kindle
-
-#--- Other ---
-# winget install --name='Microsoft Whiteboard' --exact --silent --accept-package-agreements --accept-source-agreements
