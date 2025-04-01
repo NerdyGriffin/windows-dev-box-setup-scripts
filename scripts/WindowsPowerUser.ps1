1c18d2a78b94b8ccc9da2253ef85c5fef9907c00
Christian Kunis <ckunis98@gmail.com>
2025-04-01 12:38:24 -0400

    Remove Revo in favor of Uninstalr

--- scripts/WindowsPowerUser.ps1
+++ scripts/WindowsPowerUser.ps1
@@ -2,4 +2,5 @@
 choco install -y autoruns
 winget install --id=BitSum.ProcessLasso --exact --silent --accept-package-agreements --accept-source-agreements
 choco install -y reshack
-winget install --id=RevoUninstaller.RevoUninstaller --exact --silent --accept-package-agreements --accept-source-agreements
+# winget install --id=Uninstlr.Uninstalr --exact --silent --accept-package-agreements --accept-source-agreements
+# https://uninstalr.com/
