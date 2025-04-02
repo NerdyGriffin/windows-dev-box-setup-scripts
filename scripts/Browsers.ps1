a1f47e5824b14607b3a33d7c92fbec1831403a53
Christian Kunis <ckunis98@gmail.com>
2025-04-02 11:16:11 -0400

    remove brave browser

--- scripts/Browsers.ps1
+++ scripts/Browsers.ps1
@@ -1,5 +1,5 @@
 #--- Browsers ---
-choco install -y brave
+# choco install -y brave
 # choco install -y chromium
 # choco install -y firefox
 winget install --id=Mozilla.Firefox --exact --silent --accept-package-agreements --accept-source-agreements
