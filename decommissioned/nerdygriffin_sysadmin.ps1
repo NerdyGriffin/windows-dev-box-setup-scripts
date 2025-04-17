ca7352c32b8de956c7ca432a9d78f2f894adf136
Christian Kunis <ckunis98@gmail.com>
2025-04-17 19:20:15 -0400

    Merge newer improvements into the sys-admin script

--- decommissioned/nerdygriffin_sysadmin.ps1
+++ decommissioned/nerdygriffin_sysadmin.ps1
@@ -1,3 +1,7 @@
+# Description: Boxstarter Script
+# Author: Christian Kunis (NerdyGriffin)
+# Common settings for system administration
+
 If ($Boxstarter.StopOnPackageFailure) { $Boxstarter.StopOnPackageFailure = $false }
 
 Disable-UAC
@@ -25,31 +29,43 @@ function executeScript {
 	Start-Sleep -Seconds 1;
 }
 
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+#--- Powershell Module Repository
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
 #--- Package Manager ---
 executeScript 'ConfigureChocolatey.ps1';
-executeScript 'InstallWinGet.ps1';
-executeScript 'PackageManagement.ps1';
 
 #--- Setting up Windows ---
-# executeScript 'DisableIPv6.ps1';
 executeScript 'SystemConfiguration.ps1';
-executeScript 'FileExplorerSettings.ps1';
-executeScript 'CreateBoxstarterShortcut.ps1';
-
-#--- Setting up Chocolatey
-executeScript 'ChocolateyExtensions.ps1';
-executeScript 'ChocolateyGUI.ps1';
+# executeScript 'FileExplorerSettings.ps1';
+executeScript 'CustomFileExplorerSettings.ps1'
+# executeScript 'RemoveDefaultApps.ps1';
+executeScript 'CommonDevTools.ps1';
+executeScript 'Bonjour.ps1';
+Set-TimeZone -Id "Eastern Standard Time"
 
 #--- YubiKey Authentication ---
 executeScript 'YubiKey.ps1';
 
+#--- Configure Git ---
+executeScript 'ConfigureGit.ps1';
+
+#--- Configure Powershell Profile for Powerline and PSReadline ---
+executeScript 'ConfigurePowerShell.ps1';
+
 #--- Administrative Tools ---
+executeScript 'WindowsADK.ps1';
 executeScript 'RemoteServerAdministrationTools.ps1';
-executeScript 'HardwareMonitoring.ps1';
 executeScript 'FileAndStorageUtils.ps1';
 executeScript 'SQLServerManagementStudio.ps1'
 executeScript 'NetworkTools.ps1';
-executeScript 'RemoteAndLocalFileSystem.ps1';
+
+#--- Disable Sticky keys prompt ---
+# Based on https://github.com/ChrisTitusTech/win10script
+Write-Output 'Disabling Sticky keys prompt...'
+Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\StickyKeys' -Name 'Flags' -Type String -Value '506'
 
 #--- Parse Boxstarter log for failed package installs ---
 executeScript 'ParseBoxstarterLog.ps1';
@@ -60,7 +76,18 @@ if (-not(Test-Path $SimpleLog)) {
 }
 Add-Content -Path $SimpleLog -Value 'nerdygriffin_sysadmin' | Out-Null
 
+Get-ChildItem -Path (Join-Path $env:ChocolateyInstall 'lib') | Where-Object -Property Name -Like 'tmp*.tmp' | Remove-Item -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+Get-ChildItem -Path (Join-Path $env:ChocolateyInstall 'lib-bad') | Where-Object -Property Name -Like 'tmp*.tmp' | Remove-Item -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+
 #--- reenabling critial items ---
 Enable-UAC
 Enable-MicrosoftUpdate
-Install-WindowsUpdate -acceptEula
+try {
+	Install-WindowsUpdate -acceptEula
+} catch {
+	try {
+		Install-WindowsUpdate
+	} catch {
+		# Do nothing
+	}
+}
