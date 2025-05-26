02819bf3670dd4c73816e16a37be2c8610dccd3e
Christian Kunis <ckunis98@gmail.com>
2025-05-26 14:36:24 -0400

    Update Nvidia script to use new app

--- scripts/NvidiaGraphics.ps1
+++ scripts/NvidiaGraphics.ps1
@@ -2,7 +2,7 @@ $PrevRebootState = $Boxstarter.RebootOk -or $false
 $Boxstarter.RebootOk = $false
 
 #--- Nvidia Graphics ---
-choco install -y geforce-experience
-choco install -y nvidia-profile-inspector
+choco install -y nvidia-app
+# choco install -y nvidia-profile-inspector
 
 $Boxstarter.RebootOk = $PrevRebootState
