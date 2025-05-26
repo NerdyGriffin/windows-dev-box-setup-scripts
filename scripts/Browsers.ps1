350505d0922559e2d506d88b6f032701ea729417
Christian Kunis <ckunis98@gmail.com>
2025-05-25 21:47:19 -0400

    convert browsers from choco to winget

--- scripts/Browsers.ps1
+++ scripts/Browsers.ps1
@@ -1,6 +1,4 @@
 #--- Browsers ---
-# choco install -y brave
-# choco install -y chromium
-# choco install -y firefox
+# winget install --id=Brave.Brave --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=Google.Chrome --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Mozilla.Firefox --exact --silent --accept-package-agreements --accept-source-agreements
-choco install -y googlechrome
