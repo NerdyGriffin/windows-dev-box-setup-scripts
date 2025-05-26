b683c748ade62aaa6004eba2a825e90b71b3552a
Christian Kunis <ckunis98@gmail.com>
2025-05-25 21:22:21 -0400

    Migrate serveral game launchers from choco to winget

--- scripts/GameLaunchers.ps1
+++ scripts/GameLaunchers.ps1
@@ -3,9 +3,9 @@ choco install -y cemu
 choco install -y dolphin
 choco install -y ds4windows
 winget install --id=ElectronicArts.EADesktop --exact --silent --accept-package-agreements --accept-source-agreements
-choco install -y epicgameslauncher
-choco install -y goggalaxy
-choco install -y minecraft-launcher
+winget install --id=EpicGames.EpicGamesLauncher --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=GOG.Galaxy --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=Mojang.MinecraftLauncher --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Mojang.MinecraftLauncher --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Valve.Steam  --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Ubisoft.Connect --exact --silent --accept-package-agreements --accept-source-agreements
