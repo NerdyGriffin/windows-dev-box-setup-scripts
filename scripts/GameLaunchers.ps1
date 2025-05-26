11fa5a3c487a67900c99c0aca324c29dcf8153e3
Christian Kunis <ckunis98@gmail.com>
2025-05-25 21:40:45 -0400

    convert all games launchers to winget

--- scripts/GameLaunchers.ps1
+++ scripts/GameLaunchers.ps1
@@ -1,11 +1,10 @@
 #--- Game Launchers ---
-choco install -y cemu
-choco install -y dolphin
-choco install -y ds4windows
+winget install --id=Cemu.Cemu --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=DolphinEmulator.Dolphin --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=ElectronicArts.EADesktop --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=EpicGames.EpicGamesLauncher --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=GOG.Galaxy --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Mojang.MinecraftLauncher --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Mojang.MinecraftLauncher --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id=Valve.Steam  --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id=Ubisoft.Connect --exact --silent --accept-package-agreements --accept-source-agreements
+winget install --id=Valve.Steam  --exact --silent --accept-package-agreements --accept-source-agreements
