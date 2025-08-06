e906d7f06929d2b57e17f144c85dcbde93a5f294
Christian Kunis <contact@nerdygriffin.net>
2025-08-06 15:18:36 -0400

    Re-enable TeX Live

--- scripts/OfficeTools.ps1
+++ scripts/OfficeTools.ps1
@@ -1,9 +1,12 @@
-#--- PDF ---
-choco install -y adobereader
-choco install -y pdfsam
+#--- LaTeX ---
+choco install -y texlive
 
 #--- Office Suite ---
 winget install --id=ONLYOFFICE.DesktopEditors --exact --silent --accept-package-agreements --accept-source-agreements
+
+#--- PDF ---
+choco install -y adobereader
+choco install -y pdfsam
 
 #--- Handwritten Document & Drawing ---
 choco install -y xournalplusplus
